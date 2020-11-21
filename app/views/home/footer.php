68db953f78861aa6635477027df9bc77469960d4
LucBonnet <lucbonnet10@gmail.com>
2020-11-21 01:57:34 -0300

    Avaliacao atualizacao

--- app/views/home/footer.php
+++ app/views/home/footer.php
@@ -4,7 +4,7 @@
     <div class="row align-items-center">
       <div class="col-lg-4 text-lg-left">Copyright © Jean-Luc Bonnet 2020</div>
       <div class="col-lg-4 my-3 my-lg-0">
-        <a class="btn btn-dark btn-social mx-2" href="#!">
+        <a class="btn btn-dark btn-social mx-2" href="https://github.com/LucBonnet" target="_blank">
           <img src="https://icon-library.com/images/github-icon-white/github-icon-white-6.jpg" alt="Github">
         </a>
       </div>
